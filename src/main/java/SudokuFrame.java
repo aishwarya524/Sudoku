c040813f8403d9c5bc422c9fdd04d67e82e8788f
aishwarya524 <103186648+aishwarya524@users.noreply.github.com>
2022-04-19 13:43:33 +0530

    Update SudokuFrame.java

--- src/main/java/SudokuFrame.java
+++ src/main/java/SudokuFrame.java
@@ -1,83 +1,101 @@
-package pong;
+package sudoku;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Paddle extends Rectangle{
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-    int id;
-    int yVelocity;
-    int speed = 10;
+@SuppressWarnings("serial")
 
-    public Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
-        super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
-        this.id = id;
+public class SudokuFrame extends JFrame{
+
+    private JPanel buttonSelectionPanel;
+    private SudokuPanel sPanel;
+
+    public SudokuFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Sudoku");
+        this.setMinimumSize(new Dimension(800, 600));
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu file = new JMenu("Game");
+        JMenu newGame = new JMenu("New Game");
+        JMenuItem sixBySixGame = new JMenuItem("6 By 6 Game");
+        sixBySixGame.addActionListener(new NewGameListener(SudokuPuzzleType.SIXBYSIX, 30));
+        JMenuItem nineByNineGame = new JMenuItem("9 By 9 Game");
+        nineByNineGame.addActionListener(new NewGameListener(SudokuPuzzleType.NINEBYNINE, 26));
+        JMenuItem twelveByTwelveGame = new JMenuItem("12 By 12 Game");
+        twelveByTwelveGame.addActionListener(new NewGameListener(SudokuPuzzleType.TWELVEBYTWELVE, 20));
+
+        newGame.add(sixBySixGame);
+        newGame.add(nineByNineGame);
+        newGame.add(twelveByTwelveGame);
+        file.add(newGame);
+        menuBar.add(file);
+        this.setJMenuBar(menuBar);
+
+        JPanel windowPanel = new JPanel();
+        windowPanel.setLayout(new FlowLayout());
+        windowPanel.setPreferredSize(new Dimension(800, 600));
+
+        buttonSelectionPanel = new JPanel();
+        buttonSelectionPanel.setPreferredSize(new Dimension(100, 500));
+
+        sPanel = new SudokuPanel();
+
+        windowPanel.add(sPanel);
+        windowPanel.add(buttonSelectionPanel);
+        this.add(windowPanel);
+
+        rebuildInterface(SudokuPuzzleType.NINEBYNINE, 26);
     }
 
-    public void keyPressed(KeyEvent e){
-        switch(id){
-            case 1:
-                if(e.getKeyCode()==KeyEvent.VK_W) {
-                    setYDirection(-speed);
-                    move();
-                }
-                if(e.getKeyCode()==KeyEvent.VK_S) {
-                    setYDirection(speed);
-                    move();
-                }
-                break;
-            case 2:
-                if(e.getKeyCode()==KeyEvent.VK_UP) {
-                    setYDirection(-speed);
-                    move();
-                }
-                if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-                    setYDirection(speed);
-                    move();
-                }
-                break;
+    public void rebuildInterface(SudokuPuzzleType puzzleType, int fontSize) {
+        SudokuPuzzle generatedPuzzle = new SudokuGenerator().generateRandomSudoku(puzzleType);
+        sPanel.newSudokuPuzzle(generatedPuzzle);
+        sPanel.setFontSize(fontSize);
+        buttonSelectionPanel.removeAll();
+        for (String value : generatedPuzzle.getValidValues()) {
+            JButton b = new JButton(value);
+            b.setPreferredSize(new Dimension(41, 40));
+            b.addActionListener(sPanel.new NumActionListener());
+            buttonSelectionPanel.add(b);
+        }
+        sPanel.repaint();
+        buttonSelectionPanel.revalidate();
+        buttonSelectionPanel.repaint();
+    }
+
+    private class NewGameListener implements ActionListener{
+        private SudokuPuzzleType puzzleType;
+        private int fontSize;
+
+        public NewGameListener(SudokuPuzzleType puzzleType, int fontSize){
+            this.puzzleType = puzzleType;
+            this.fontSize = fontSize;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e){
+            rebuildInterface(puzzleType, fontSize);
         }
     }
 
-    public void keyReleased(KeyEvent e){
-        switch(id){
-            case 1:
-                if(e.getKeyCode()==KeyEvent.VK_W) {
-                    setYDirection(0);
-                    move();
-                }
-                if(e.getKeyCode()==KeyEvent.VK_S) {
-                    setYDirection(0);
-                    move();
-                }
-                break;
-            case 2:
-                if(e.getKeyCode()==KeyEvent.VK_UP) {
-                    setYDirection(0);
-                    move();
-                }
-                if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-                    setYDirection(0);
-                    move();
-                }
-                break;
-        }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                SudokuFrame frame = new SudokuFrame();
+                frame.setVisible(true);
+            }
+        });
     }
-
-    public void setYDirection(int yDirection){
-        yVelocity = yDirection;
-    }
-
-    public void move(){
-        y = y + yVelocity;
-    }
-
-    public void draw(Graphics g){
-        if(id==1)
-            g.setColor(Color.BLUE);
-        else
-            g.setColor(Color.RED);
-        g.fillRect(x, y, width, height);
-    }
-
 }
